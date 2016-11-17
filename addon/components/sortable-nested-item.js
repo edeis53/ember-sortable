b2645c744406e2eed179974271e1ca85040be5f9
Eric Deis <edeis@EricsMacBook-Air.local>
2016-11-17 14:16:21 -0800

    Fix recession issues with template.

--- addon/components/sortable-nested-item.js
+++ addon/components/sortable-nested-item.js
@@ -3,5 +3,6 @@ import layout from '../templates/components/sortable-nested-item';
 import SortableNestedItemMixin from '../mixins/sortable-nested-item';
 
 export default Ember.Component.extend(SortableNestedItemMixin, {
-  layout
+  layout,
+  tagName: 'div'
 });
